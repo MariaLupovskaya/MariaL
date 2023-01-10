8ac72bd3bfdf5a0bef36d7ace7b02c23e48779ac
Maria <veiershtrass@inbox.ru>
2023-01-10 19:30:35 +0300

    Программа с решением задачи

---
+++ Exam/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] array = {"Hello", "2", "world", ":-)"};
+int x = array.Length;
+string result = "";
+for (int i = 0; i < x; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result = array[i];
+        Console.Write(result + " ");
+    }
+}
